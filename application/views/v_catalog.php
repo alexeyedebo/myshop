35397b5ee851e19fc9a03127dace029190a11847
Alexey <alexey.edebo@gmail.com>
2016-08-16 11:49:03 +0300

    mvc

--- application/views/v_catalog.php
+++ application/views/v_catalog.php
@@ -1,1 +1,16 @@
-<?php
+<table border='1px' width = '100%'>
+    <thead>
+    <tr>
+        <td>Наименование</td>
+        <td>Цена</td>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $product => $cost):?>
+    <tr>
+        <td><?=$product?></td>
+        <td><?=$cost?></td>
+    </tr>
+    <?php endforeach ?>
+    </tbody>
+</table>
