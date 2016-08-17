42a24f7c3d142fb3866a742b4fd7876ce6bec3a2
Alexey <alexey.edebo@gmail.com>
2016-08-17 16:43:21 +0300

    two commit without modules pagination, mail ...

---
+++ application/logs/2016/08/17.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2016-08-17 13:09:21 --- EMERGENCY: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'pagination' at 'MODPATH/pagination' ~ SYSPATH/classes/Kohana/Core.php [ 579 ] in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 13:09:21 --- DEBUG: #0 /home/alexey/dev/phpacademy/myshop/application/bootstrap.php(123): Kohana_Core::modules(Array)
+#1 /home/alexey/dev/phpacademy/myshop/index.php(102): require('/home/alexey/de...')
+#2 {main} in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 13:14:52 --- EMERGENCY: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'orm-mptt' at 'MODPATH/orm-mptt' ~ SYSPATH/classes/Kohana/Core.php [ 579 ] in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 13:14:52 --- DEBUG: #0 /home/alexey/dev/phpacademy/myshop/application/bootstrap.php(123): Kohana_Core::modules(Array)
+#1 /home/alexey/dev/phpacademy/myshop/index.php(102): require('/home/alexey/de...')
+#2 {main} in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 13:27:32 --- EMERGENCY: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'orm-mptt' at 'MODPATH/orm-mptt' ~ SYSPATH/classes/Kohana/Core.php [ 579 ] in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 13:27:32 --- DEBUG: #0 /home/alexey/dev/phpacademy/myshop/application/bootstrap.php(123): Kohana_Core::modules(Array)
+#1 /home/alexey/dev/phpacademy/myshop/index.php(102): require('/home/alexey/de...')
+#2 {main} in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 16:13:11 --- EMERGENCY: Kohana_Exception [ 0 ]: Attempted to load an invalid or missing module 'pagination' at 'MODPATH/pagination' ~ SYSPATH/classes/Kohana/Core.php [ 579 ] in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
+2016-08-17 16:13:11 --- DEBUG: #0 /home/alexey/dev/phpacademy/myshop/application/bootstrap.php(123): Kohana_Core::modules(Array)
+#1 /home/alexey/dev/phpacademy/myshop/index.php(102): require('/home/alexey/de...')
+#2 {main} in /home/alexey/dev/phpacademy/myshop/application/bootstrap.php:123
